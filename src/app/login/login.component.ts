6f51c701ee905e7675948c6b8425ff77497e96dd
Javier Sabadell Montero <sabadellmonterojavier@gmail.com>
2025-02-24 12:44:01 +0100

    Updated login component link

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { FirebaseService } from '../firebase.service';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router'; // Importa Router
+import { Router, RouterModule } from '@angular/router'; // Importa Router
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, CommonModule ],
+  imports: [FormsModule, CommonModule, RouterModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
