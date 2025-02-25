4214cecc05f8bf762c9ef8d76123465b4ec00e6b
H <hugoestudiando1@gmail.com>
2025-02-25 13:34:59 +0100

    updated styles

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -3,105 +3,106 @@ import { Component, Input, ElementRef, OnChanges, SimpleChanges, ViewChild } fro
 import * as d3 from 'd3';
 import { CommonModule } from '@angular/common';
 
-// Define la interfaz para los datos del gráfico de barras
 export interface BarChartDataPoint {
-  category: string; // Changed from month to category
-  total: number;
-  client?: string;
-  issuer?: string;
-  [key: string]: any; // Index signature: Allow string indexing
+  category: string;
+  total: number;
+  [key: string]: any; // Index signature: Allow accessing properties by string
 }
 
 @Component({
-  selector: 'app-bar-chart',
-  standalone: true,
-  imports: [CommonModule],
-  template: '<svg #chart></svg>',
-  styleUrl: './bar-chart.component.css'
+  selector: 'app-bar-chart',
+  standalone: true,
+  imports: [CommonModule],
+  template: '<svg #chart></svg>',
+  styleUrl: './bar-chart.component.css'
 })
 export class BarChartComponent implements OnChanges {
-  @ViewChild('chart', { static: true }) chartRef!: ElementRef;
-  @Input() data: BarChartDataPoint[] = [];
-  @Input() categoryField: string = '';
-  @Input() valueField: string = '';
-  @Input() chartTitle: string = '';
+  @ViewChild('chart', { static: true }) chartRef!: ElementRef;
+  @Input() data: BarChartDataPoint[] = [];
+  @Input() categoryField: string = 'category';
+  @Input() valueField: string = 'total';
+  @Input() chartTitle: string = '';
 
-  private svg: any;
-  private margin = { top: 60, right: 20, bottom: 50, left: 70 };
-  private width = 600 - this.margin.left - this.margin.right;
-  private height = 400 - this.margin.top - this.margin.bottom;
+  private svg: any;
+  private margin = { top: 60, right: 20, bottom: 50, left: 70 };
+  private width = 600 - this.margin.left - this.margin.right;
+  private height = 400 - this.margin.top - this.margin.bottom;
 
-  constructor() { }
+  constructor() { }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['data'] && this.data) {
-      this.createChart();
-    }
-  }
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['data'] && this.data) {
+      this.createChart();
+    }
+  }
 
-  createChart(): void {
-    if (!this.data || this.data.length === 0) {
-      return;
-    }
+  createChart(): void {
+    if (!this.data || this.data.length === 0) {
+      return;
+    }
 
-    this.width = 600 - this.margin.left - this.margin.right;
-    this.height = 400 - this.margin.top - this.margin.bottom;
+    console.log("Data in createChart:", this.data);
 
-    this.svg = d3.select(this.chartRef.nativeElement)
-      .attr('width', this.width + this.margin.left + this.margin.right)
-      .attr('height', this.height + this.margin.top + this.margin.bottom);
+    this.width = 600 - this.margin.left - this.margin.right;
+    this.height = 400 - this.margin.top - this.margin.bottom;
 
-    this.svg.selectAll('*').remove();
+    this.svg = d3.select(this.chartRef.nativeElement)
+      .attr('width', this.width + this.margin.left + this.margin.right)
+      .attr('height', this.height + this.margin.top + this.margin.bottom);
 
-    const g = this.svg.append("g")
-      .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+    this.svg.selectAll('*').remove();
 
-    g.append("text")
-      .attr("x", this.width / 2)
-      .attr("y", -this.margin.top / 2)
-      .attr("text-anchor", "middle")
-      .style("font-size", "16px")
-      .style("font-weight", "bold")
-      .text(this.chartTitle);
+    const g = this.svg.append("g")
+      .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+
+    g.append("text")
+      .attr("x", this.width / 2)
+      .attr("y", -this.margin.top / 2)
+      .attr("text-anchor", "middle")
+      .style("font-size", "16px")
+      .style("font-weight", "bold")
+      .text(this.chartTitle);
+
+    const x = d3.scaleBand()
+      .domain(this.data.map(d => d[this.categoryField]))
+      .rangeRound([0, this.width])
+      .padding(0.1);
+
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(this.data, d => d[this.valueField] || 0)])
+      .range([this.height, 0]);
+
+    g.append("g")
+      .attr("transform", "translate(0," + this.height + ")")
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", ".15em")
+      .attr("transform", "rotate(-45)");
+
+    g.append("g")
+      .call(d3.axisLeft(y))
+      .append("text")
+      .attr("fill", "#000")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "-4.5em")
+      .attr("text-anchor", "end")
+      .attr("font-weight", "bold")
+      .text("Total (€)");
 
 
-    const x = d3.scaleBand()
-      .domain(this.data.map(d => d[this.categoryField]))
-      .rangeRound([0, this.width])
-      .padding(0.1);
-
-    const y = d3.scaleLinear()
-      .domain([0, d3.max(this.data, d => d[this.valueField])])
-      .range([this.height, 0]);
-
-    g.append("g")
-      .attr("transform", "translate(0," + this.height + ")")
-      .call(d3.axisBottom(x))
-      .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", ".15em")
-      .attr("transform", "rotate(-45)");
-
-    g.append("g")
-      .call(d3.axisLeft(y))
-      .append("text")
-      .attr("fill", "#000")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "-4.5em")
-      .attr("text-anchor", "end")
-      .attr("font-weight", "bold")
-      .text("Total (€)");
-
-    g.selectAll(".bar")
-      .data(this.data)
-      .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", (d: BarChartDataPoint) => x(d[this.categoryField]))
-      .attr("y", (d: BarChartDataPoint) => y(d[this.valueField]))
-      .attr("width", x.bandwidth())
-      .attr("height", (d: BarChartDataPoint) => this.height - y(d[this.valueField]))
-      .attr("fill", "steelblue");
-  }
+    g.selectAll(".bar")
+      .data(this.data)
+      .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", (d: BarChartDataPoint) => d?.[this.categoryField] ? x(d[this.categoryField]) : 0)
+      .attr("y", (d: BarChartDataPoint) => y(d[this.valueField]))
+      .attr("width", x.bandwidth())
+      .attr("height", (d: BarChartDataPoint) => this.height - y(d[this.valueField]))
+      .attr("fill", "#f9e3b7")
+      .attr("stroke", "#b16c34")
+      .attr("stroke-width", 1);
+  }
 }
